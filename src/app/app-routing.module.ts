e98e100d5bc4a89095790fd195b1a9d446de1ea7
mcuin <mykcuin@gmail.com>
2019-04-02 18:53:36 -0600

    Routing in place.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {CuinSolutionsComponent} from './cuin-solutions/cuin-solutions.component'
+import {MacrosManagerComponent} from './macros-manager/macros-manager.component'
+import {BoozyDiceComponent} from './boozy-dice/boozy-dice.component'
+import { from } from 'rxjs';
+
+const routes: Routes = [
+  {path: '', component: CuinSolutionsComponent},
+  {path: 'macrosmanager', component: MacrosManagerComponent},
+  {path: 'boozydice', component: BoozyDiceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
